08c27e08d0945b9207895227683e2fc2f53091e6
Mitushi-23 <mitushi2agrawal@gmail.com>
2023-04-10 16:43:01 +0530

    added props

--- src/App.js
+++ src/App.js
@@ -1,31 +1,53 @@
-import React from 'react'
-import Cards from './component/Cards'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Form from 'react-bootstrap/Form';
-import './App.css';
+import React from "react";
+import Cards from "./component/Cards";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Form from "react-bootstrap/Form";
+import "./App.css";
+import sad from "./assets/sad.jpeg";
+import happy from "./assets/happy.jpeg";
 
 const App = () => {
   return (
     <>
-    <div style={{marginTop:'5%'}}>
-    <h1 style={{textAlign:'center'}}>Sentiment analysis of a paragraph</h1>
-    <Form>
-
-    <Form.Control
-        type="text"
-        placeholder="Enter text"
-        aria-label="Text"
-        style={{width:'50%',margin:'auto',marginTop:'2%'}}
-        />
+      <div style={{ marginTop: "5%" }}>
+        <h1 style={{ textAlign: "center" }}>
+          Sentiment analysis of a paragraph
+        </h1>
+        <Form>
+          <Form.Control
+            type="text"
+            placeholder="Enter text"
+            aria-label="Text"
+            style={{ width: "50%", margin: "auto", marginTop: "2%" }}
+          />
         </Form>
-    <div style={{display:'flex', justifyContent:'space-around',width:'80%',margin:'auto',marginTop:'6%'}}>
-
-    <Cards title="Good"/>
-    <Cards title="Bad"/>
-    </div>
-    </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-around",
+            width: "80%",
+            margin: "auto",
+            marginTop: "6%",
+          }}
+        >
+          <Cards
+            title="Good"
+            img={happy}
+            result="POSITIVE"
+            resultbg="#0bb39830"
+            resultcol="#0bb398"
+          />
+          <Cards
+            title="Bad"
+            img={sad}
+            result="NEGATIVE"
+            resultbg="#ff000036"
+            resultcol="red"
+          />
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/component/Cards.jsx
+++ src/component/Cards.jsx
@@ -1,21 +1,33 @@
-import React from 'react'
+import React from "react";
 // import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import Card from "react-bootstrap/Card";
+import sad from "../assets/sad.jpeg";
 
-const Cards = ({props}) => {
+const Cards = (props) => {
   return (
     <>
-    <Card style={{ width: '18rem'}}>
-      
-    <Card.Img variant="top" src="https://spng.pngfind.com/pngs/s/609-6093177_smiling-face-png-pic-smiley-face-emoji-with.png" />
-    <Card.Body>
-      <Card.Title  style={{textAlign:'center'}}></Card.Title>
-     <h5 style={{textAlign:'center',padding:'6px',width:'50%',margin:'auto',borderRadius:'6px',color:'#0bb398',backgroundColor:'#0bb39830',fontWeight:'600'}}>POSITIVE</h5>
-      {/* <Button variant="primary">Go somewhere</Button> */}
-    </Card.Body>
-  </Card>
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src={props.img} />
+        <Card.Body>
+          <Card.Title style={{ textAlign: "center" }}>{props.title}</Card.Title>
+          <h5
+            style={{
+              textAlign: "center",
+              padding: "6px",
+              width: "50%",
+              margin: "auto",
+              borderRadius: "6px",
+              color: props.resultcol,
+              backgroundColor: props.resultbg,
+              fontWeight: "600",
+            }}
+          >
+            {props.result}
+          </h5>
+        </Card.Body>
+      </Card>
     </>
-  )
-}
+  );
+};
 
-export default Cards
+export default Cards;
